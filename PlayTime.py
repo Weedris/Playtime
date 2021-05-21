700bcc158cc8f5cba124042699ab5c445f83c01d
Weedris <jud.thivet@gmail.com>
2021-05-21 18:32:42 +0200

    add first iteration of tictactoe game and small changes

--- PlayTime.py
+++ PlayTime.py
@@ -1,6 +1,7 @@
 from os import curdir
 import discord
 import json
+import random
 from discord.ext import commands
 from discord.utils import find
 
@@ -51,14 +52,14 @@ async def storyList(ctx):
         for story in data[ "stories" ]:
             out += " - " + story ["name"] + "\n"
     
-    await ctx.channel.send("```" + "Voici la liste des histoires disponnible\n" + out + "```")
+    await ctx.channel.send("```" + "Voici la liste des histoires disponnible :\n" + out + "```")
 
 
 #-----------------------------------------------------------------------------
 @client.command(
     name = "storyTime",
-    help = "write !pt storyTime 'nom d'histoire', afin de commencer une histoire interactive",
-    brief = "start a stories where You are the hero"
+    help = "write .pt storyTime 'nom d'histoire', afin de commencer une histoire interactive.",
+    brief = "start a stories where You are the hero."
 )
 async def storyTime(ctx, arg):
     await ctx.channel.send("It's stories time then...\nN'oublie pas il faut juste repondre un chiffre")
@@ -80,9 +81,9 @@ async def storyTime(ctx, arg):
     with open("stories.json", "r") as pannelson:
         data = json.load( pannelson )
         for story in data[ "stories" ]:
-            if story["name"] == arg:
-                for panel in story["panel"]:
-                    panels.append( Panel( panel["choices"], panel["content"] ) )
+            if story[ "name" ] == arg:
+                for panel in story[ "panel" ]:
+                    panels.append( Panel( panel[ "choices" ], panel[ "content" ] ) )
     
     curent = panels[0]
     
@@ -94,5 +95,104 @@ async def storyTime(ctx, arg):
     await ctx.channel.send(curent.__str__())
     await ctx.channel.send("Merci d'avoir joue cette histoire interactive")
 
+#-----------------------------------------------------------------------------
+@client.command(
+    name = "tikTakToe",
+    help = "The bot play a game of TikTakToe against you, to play just enter the coordinate of your move.",
+    brief = "The bot play a game of TikTakToe against you."
+)
+async def tikTakToe(ctx, arg):
+    await ctx.channel.send("Let's play a little game together")
+    author = ctx.author
+
+    gameBoard = [
+        [ 0, 0, 0],
+        [ 0, 0, 0],
+        [ 0, 0, 0]
+    ]
+
+    starter = random.randint(0, 1)
+    player = starter
+
+    # send the board inhto the channel where the game is held
+    def sendBoard(board):
+        out = ""
+        for line in board:
+            for element in line:
+                out += " | "
+                if element == "0":
+                    out += " "
+                elif element == "1":
+                    out += "X"
+                else:
+                    out += "O"
+                
+            out += "\n"
+        ctx.channel.send(out)
+    
+    # play the play from the player return true -> it went fine
+    def play(player, x, y):
+        if gameBoard[ x - 1, y - 1 ] == 0:
+            gameBoard[ x - 1, y - 1 ] = player
+            return True
+        else:
+            return False
+    
+    # verify if someone win the game or if the game is a draw
+    def verify():
+        
+        # check lines
+        for line in gameBoard:
+            if line == [ 1, 1, 1]:
+                return 1
+            elif line == [ 2, 2, 2]:
+                return 2
+        
+        # check columns
+        columns = [ [], [], [] ]
+        for line in range( len(gameBoard) ):
+            for column in range( len( gameBoard[0] ) ):
+                columns[ column ].append( gameBoard[ line ][ column ] )
+        
+        for column in columns:
+            if column == [ 1, 1, 1]:
+                return 1
+            elif column == [ 2, 2, 2]:
+                return 2
+        
+        # check diags
+        diags = [ [], [] ]
+        for line in range( len(gameBoard) ):
+            for column in range( len( gameBoard[0] ) ):
+                if line == column:
+                    diags[ 0 ].append( gameBoard[ line ][ column ] )
+                elif line + column == 2:
+                    diags[ 1 ].append( gameBoard[ line ][ column ] )
+        
+        for diag in diags:
+            if diag == [ 1, 1, 1]:
+                return 1
+            elif diag == [ 2, 2, 2]:
+                return 2
+        
+        # check if draw
+        draw = True
+        for line in range( len(gameBoard) ):
+            for column in range( len( gameBoard[0] ) ):
+                if gameBoard[ line ][ column ] == 0:
+                    draw = False
+                    break
+        
+        if draw:
+            return 3
+        else:
+            return 0
+    
+    # random play from the bot
+    def playFromBot():
+        pass
+    
+    while verify == 0:
+        pass
 
 client.run(token)
